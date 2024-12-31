e3eeb0f83b5020398f332e4ea8a9da20c5ee59eb
Victor Graf <victor@subgraf.dev>
2024-12-30 18:59:53 -0800

    fix derive macro to use reference when appropriate

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -6,6 +6,29 @@ use proc_macro_crate::FoundCrate;
 use quote::{quote, quote_spanned};
 use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput, Fields, Type};
 
+// TODO: Fill this in, copying from below.
+fn is_primitive_type(ty: &syn::TypePath) -> bool {
+    match ty.path.get_ident() {
+        Some(id) => matches!(
+            id.to_string().as_ref(),
+            "u8" | "u16"
+                | "u32"
+                | "u64"
+                | "u128"
+                | "usize"
+                | "i8"
+                | "i16"
+                | "i32"
+                | "i64"
+                | "i128"
+                | "isize"
+                | "bool"
+                | "char"
+        ),
+        None => false,
+    }
+}
+
 #[proc_macro_derive(Attributes)]
 pub fn derive_attributes(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -29,35 +52,45 @@ pub fn derive_attributes(input: TokenStream) -> TokenStream {
         unimplemented!("Not implemented for empty structs");
     }
 
-    let field_names: Vec<&Ident> = fields.iter().map(|f| f.ident.as_ref().unwrap()).collect();
+    let visit_field_args: Vec<_> = fields
+        .iter()
+        .map(|f| {
+            let ident = f.ident.as_ref().unwrap();
+            match f.ty {
+                Type::Path(ref p) => match is_primitive_type(p) {
+                    true => quote_spanned!(f.span() => #ident),
+                    false => quote_spanned!(f.span() => &#ident),
+                },
+                _ => quote_spanned!(f.ty.span() => compile_error!("Only type paths are supported")),
+            }
+        })
+        .collect();
 
-    // Collect all the unique types found on fields in the struct.
-    // TODO: This does not have very strong guarentees that something sensible will be generated.
-    let field_types: HashSet<Type> = fields.iter().map(|f| f.ty.clone()).collect();
-
-    let visitor_type_bounds: Vec<_> = field_types
+    // Collect all the unique types found on fields in the struct, and render them into the type
+    // bounds that will be applied to the visitor type on the Attributes implementation.
+    let visitor_type_bounds: Vec<_> = fields
+        .iter()
+        .map(|f| f.ty.clone())
+        .collect::<HashSet<_>>()
         .into_iter()
         .map(|ty| match ty {
-            Type::Path(ref p) => match p.path.get_ident() {
+            Type::Path(ref p) => match is_primitive_type(p) {
                 // Generate a type bound on e.g. Visitor<u64> for small primitive types
                 // Visitor<&T> for all other types.
-                Some(id) => match id.to_string().as_ref() {
-                    "u8" | "u16" | "u32" | "u64" | "u128" | "usize" |
-                    "i8" | "i16" | "i32" | "i64" | "i128" | "isize" | "bool" | "char" => {
-                        quote_spanned!(ty.span() => #rkvc_path::attributes::Visitor<#ty>)
-                    },
-                    _ => quote_spanned!(ty.span() => for<'a> #rkvc_path::attributes::Visitor<&'a #ty>),
-                },
-                None => quote_spanned!(ty.span() => for<'a> #rkvc_path::attributes::Visitor<&'a #ty>),
+                true => quote_spanned!(ty.span() => #rkvc_path::attributes::Visitor<#ty>),
+                false => {
+                    quote_spanned!(ty.span() => for<'a> #rkvc_path::attributes::Visitor<&'a #ty>)
+                }
             },
             _ => quote_spanned!(ty.span() => compile_error!("Only type paths are supported")),
         })
         .collect();
 
     // TODO: Add a #[label = "foo"] attribute that can be used to specify the field label manually.
-    let indices: Vec<usize> = (0..field_names.len()).collect();
-    let field_labels: Vec<_> = field_names
+    let indices: Vec<usize> = (0..fields.len()).collect();
+    let field_labels: Vec<_> = fields
         .iter()
+        .map(|f| f.ident.as_ref().unwrap())
         .map(|name| format!("{}::{}", struct_name, name))
         .collect();
 
@@ -82,7 +115,7 @@ pub fn derive_attributes(input: TokenStream) -> TokenStream {
             {
             fn elem_at(&self, i: usize, visitor: &mut V) -> Option<V::Output> {
                 match i {
-                    #(#indices => Some(visitor.visit(&self.#field_names)),)*
+                    #(#indices => Some(visitor.visit(self.#visit_field_args)),)*
                     _ => None,
                 }
             }
