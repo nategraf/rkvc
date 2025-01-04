ca9c26a7c14192b2ea31d0e62ace07b3aedfb571
Victor Graf <victor@subgraf.dev>
2025-01-03 16:36:10 -0800

    remove a todo

--- src/cmz.rs
+++ src/cmz.rs
@@ -494,7 +494,6 @@ mod test {
 
         // Client creates a commitment, has the MAC generated over it, then removes the blind from
         // the MAC. This should generate a MAC that is the same (except U) as a plaintext.
-        // TODO: This needs to commit using the public_parameters.
         let (commit, blind) =
             PedersonGenerators::from(pp.clone()).commit(&example, &mut rand::thread_rng());
         let mut mac = key.blind_mac(&commit);
